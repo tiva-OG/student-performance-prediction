2694e6b1d6ac668594081aeecf9da0261442a995
tiva-OG <tivasalvation@gmail.com>
2024-04-24 15:22:42 +0100

    created the project setup and requirements

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import find_packages, setup
+from typing import List
+
+HYPHEN_DOT = "-e ."
+
+
+def get_requirements(file_path: str) -> List[str]:
+    """
+    this function returns the list of requirements
+    """
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace("\n", "") for req in requirements]
+
+        if HYPHEN_DOT in requirements:
+            requirements.remove(HYPHEN_DOT)
+
+    return requirements
+
+
+setup(
+    name="mlproject",
+    version="0.0.1",
+    author="tiva",
+    author_email="salvationtiva@gmail.com",
+    packages=find_packages(),
+    install_requires=get_requirements("requirements.txt")
+)
