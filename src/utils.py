0e4453feac6627b89e0dd54d6416929b66283174
tiva-OG <tivasalvation@gmail.com>
2024-04-26 12:28:51 +0100

    predict pipeline

--- src/utils.py
+++ src/utils.py
@@ -50,3 +50,11 @@ def evaluate_models(X_train, y_train, X_test, y_test, models, params):
 
     except Exception as e:
         raise CustomException(e, sys)
+
+
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file:
+            return dill.load(file)
+    except Exception as e:
+        raise CustomException(e, sys)
